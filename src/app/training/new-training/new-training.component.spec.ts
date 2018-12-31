5792800a70f59ece1ee750d60dfe6eb054b92e1e
knarfben <knarfben110@gmail.com>
2018-12-31 01:37:05 +0100

    Add comment to test push credentials

--- src/app/training/new-training/new-training.component.spec.ts
+++ src/app/training/new-training/new-training.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { NewTrainingComponent } from './new-training.component'
-
+// will commit
 describe('NewTrainingComponent', () => {
   let component: NewTrainingComponent
   let fixture: ComponentFixture<NewTrainingComponent>
