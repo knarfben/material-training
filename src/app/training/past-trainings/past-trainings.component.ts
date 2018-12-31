d95cb51275f942a66551e8b75d9c138d15e5db6d
knarfben <knarfben110@gmail.com>
2018-12-31 12:45:00 +0100

    Add sorting and filtering to exercises table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core'
-import { MatTableDataSource } from '@angular/material'
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core'
+import { MatTableDataSource, MatSort } from '@angular/material'
 import { Exercise } from '../exercise.model'
 import { TrainingService } from '../training.service'
 
@@ -8,12 +8,23 @@ import { TrainingService } from '../training.service'
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.scss']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state']
   dataSource = new MatTableDataSource<Exercise>()
+
+  @ViewChild(MatSort) sort: MatSort
+
   constructor(private trainingService: TrainingService) {}
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises()
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase()
   }
 }
