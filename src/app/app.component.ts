200991e9e7233a12b2bdba360056ee7730dd483d
knarfben <knarfben110@gmail.com>
2019-01-02 19:32:13 +0100

    Add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    // no need to unsubscribe since this is a app lifetime subscription
     this.authService.initAuthListener()
   }
 }
