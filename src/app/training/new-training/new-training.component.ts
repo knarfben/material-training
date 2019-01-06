ad45c7d00a57b2d340956a0fb1f90e85a5a8950e
Frank Benady <knarfben110@gmail.com>
2019-01-06 11:14:38 +0100

    Add spinner simple way

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core'
 import { TrainingService } from '../training.service'
 import { Subscription } from 'rxjs'
 import { Exercise } from '../exercise.model'
+import { UIService } from 'src/app/shared/ui.service'
 
 @Component({
   selector: 'app-new-training',
@@ -12,15 +13,31 @@ export class NewTrainingComponent implements OnInit, OnDestroy {
   torture: string
   exercises: Exercise[]
   changedExercisesSubscription: Subscription
-  constructor(private trainingService: TrainingService) {}
+  dataLoadingSubs: Subscription
+  spinMe = true
+  constructor(
+    private trainingService: TrainingService,
+    private uiService: UIService
+  ) {}
 
   ngOnInit() {
+    console.log('Beginning of ngInit spinMe = ' + this.spinMe)
+
+    // this.dataLoadingSubs = this.uiService.loadingStateChanged.subscribe(
+    //   isLoading => {
+    //     this.spinMe = isLoading
+    //   }
+    // )
+    // this.uiService.loadingStateChanged.next(true)
     this.changedExercisesSubscription = this.trainingService.changedExercises.subscribe(
       exs => {
         this.exercises = exs
+        this.spinMe = false
+        // this.uiService.loadingStateChanged.next(false)
       }
     )
     this.trainingService.fetchAvailableExercises()
+    console.log('End of ngInit spinMe = ' + this.spinMe)
   }
 
   ngOnDestroy() {
