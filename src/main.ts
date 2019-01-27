bad7c8e8f6a1a623bfcd0489ca8505b81ed259e3
Frank Benady <knarfben110@gmail.com>
2019-01-27 03:03:59 +0100

    jit translation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,35 @@
-import 'hammerjs';
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'hammerjs'
+import {
+  enableProdMode,
+  TRANSLATIONS,
+  TRANSLATIONS_FORMAT
+} from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from './app/app.module'
+import { environment } from './environments/environment'
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// use the require method provided by webpack
+declare const require
+
+// we use the webpack raw-loader to return the content as a string
+const translations = require(`raw-loader!./locale/messages.fr.xlf`)
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    providers: [
+      {
+        provide: TRANSLATIONS,
+        useValue: translations
+      },
+      {
+        provide: TRANSLATIONS_FORMAT,
+        useValue: 'xlf'
+      }
+    ]
+  })
+  .catch(err => console.error(err))
